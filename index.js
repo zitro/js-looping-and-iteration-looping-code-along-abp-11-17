0d219d4263836a6b546d047668179f15130ea7bf
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 04:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 function tailsNeverFails () {
   let tails = 0;
 
-  while(Math.random() >= 0.5){
-  tails++;
-}
-return 'You got ${tails} tails in a row!';
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
 }
