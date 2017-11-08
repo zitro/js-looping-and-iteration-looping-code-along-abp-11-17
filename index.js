cd0220c0a62879e700b0f9968c43d7ccc4195ed4
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 05:12:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges (people) {
+  for (let i = 0; i < people.length; i++) {
+    console.log(`Welcome ${people[i]}! You are employee #${i + 1}.`);
+  }
+
+  return people;
+}
+
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
